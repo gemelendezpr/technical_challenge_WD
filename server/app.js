ca61f04bba372709df9370420b070f4369e1f50c
gerarmc <gerardomelendezpro@gmail.com>
2024-01-25 16:05:36 -0400

    app running

---
+++ server/app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+var cors = require('cors');
+var mongoose = require('mongoose')
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('trust proxy', 1);
+app.enable('trust proxy');
+
+app.use(
+    cors({
+      origin: [process.env.REACT_APP_URI]  // <== URL of our future React app
+    })
+  );
+
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.get("/phones", (req, res) => {
+    res.json(phones);
+  });
+
+
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+module.exports = app;
