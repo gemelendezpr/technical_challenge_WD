591f44c7bccd48f2843d77031e996843e88a7e1b
gerarmc <gerardomelendezpro@gmail.com>
2024-01-25 22:46:05 -0400

    basic styling added

--- client/pages/Phones.jsx
+++ client/pages/Phones.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const SERVER_URL = 'http://localhost:4000';
 
 const Phones = () => {
-  const [ phones, setPhones ] = useState([]);
+  const [phones, setPhones] = useState([]);
   const [loading, setLoading] = useState(true);
 
   // Get all the phones from the server
@@ -13,26 +13,25 @@ const Phones = () => {
     axios
       .get(`${SERVER_URL}/phones`)
       .then((response) => {
-          console.log(response.data);
-          setPhones(response.data);
+        console.log(response.data);
+        setPhones(response.data);
       })
       .catch((error) => console.log(error));
-      
+
     setLoading(false);
-  }
+  };
 
   useEffect(() => {
     getPhones();
   }, []);
 
-  // Get all the phones image from the assets
+  // Get the path for phone images
   const getImagePath = (imageName) => `../assets/images/${imageName}` || '';
 
   return (
-    <div>
-      <h1>All Phones</h1>
+    <div style={{ maxWidth: "1200px", margin: "0 auto", display: "flex", flexWrap: "wrap", justifyContent: "space-between" }}>
+      <h1>Most Popular Phones</h1>
 
-      {/* First it waits to get the inf the it displays all phones as links in the page */}
       {loading ? (
         <div className="d-flex justify-content-center">
           <div className="spinner-border" role="status">
@@ -40,22 +39,33 @@ const Phones = () => {
           </div>
         </div>
       ) : (
-        <div className="phoneList">
-        
-        {phones &&
-        phones.map((phone, index) => (
-          <div key={index}>
-            <Link to={`/phones/${phone.id}`}>
-              <div>
-                <img src={getImagePath(phone.imageFileName)} alt={phone.name} width={'200vw'} height={'200vh'} />
+        <div className="cardContainer" style={{ display: "flex", flexWrap: "wrap",justifyContent: "space-between", marginLeft:"0%" }}>
+          {phones &&
+            phones.map((phone, index) => (
+              <div key={index} className="card" style={{ width: "23%", marginBottom: "1rem", display: "flex", flexDirection: "column", borderRadius: "10px" }}>
+                <Link to={`/phones/${phone.id}`} style={{ flex: 1 }}>
+                  <div className="card-img-container">
+                    <img
+                      src={getImagePath(phone.imageFileName)}
+                      alt={phone.name}
+                      className="card-img-top"
+                    />
+                  </div>
+                </Link>
+                <div className="card-body" style={{ flex: 1 }}>
+                  <h5 className="card-title">{phone.name}</h5>
+                  <p className="card-text">Manufacturer: {phone.manufacturer}</p>
+                  <p className="card-text">Price: ${phone.price}</p>
+                  <Link to={`/phones/${phone.id}`} className="btn btn-primary" style={{ alignSelf: "flex-end" }}>
+                    View Details
+                  </Link>
+                </div>
               </div>
-            </Link>
-          </div>
-        ))}
+            ))}
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Phones
+export default Phones;
--- client/pages/PhonesInfo.jsx
+++ client/pages/PhonesInfo.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
-import axios from "axios"
+import axios from "axios";
 
 const SERVER_URL = 'http://localhost:4000';
 
 const PhonesInfo = () => {
-  const [ phone, setPhone ] = useState([]);
+  const [phone, setPhone] = useState([]);
   const { phoneId } = useParams();
   const navigate = useNavigate();
 
@@ -27,22 +27,32 @@ const PhonesInfo = () => {
   const getImagePath = (imageName) => `../assets/images/${imageName}` || ''; 
 
   return (
-    <div>
-      {/* It will display all the info on the page */}
-      <img src={getImagePath(phone.imageFileName)} alt={phone.name} width={'200vw'} height={'200vh'}/>
-      <h1>{phone.name}</h1>
-
-      <p>Manufacturer: {phone.manufacture}</p>
-      <p>Description: {phone.description}</p>
-      <p>Color: {phone.color}</p>
-      <p>Price: ${phone.price}</p>
-      <p>Screen: {phone.screen}</p>
-      <p>Processor: {phone.processor}</p>
-      <p>Ram: {phone.ram}</p>
-
-      <button onClick={() => navigate(-1)}>Back</button>
+    <div style={{ height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+    {phone && (
+      <div className="card" style={{ width: "60%", textAlign: "center" }}>
+        <img
+           src={getImagePath(phone.imageFileName)}
+           alt={phone.name}
+           className="card-img-top"
+           style={{ objectFit: "contain", width: "100%", height: "200px" }}
+        />
+          <div className="card-body">
+            <h5 className="card-title">{phone.name}</h5>
+            <p className="card-text">Manufacturer: {phone.manufacturer}</p>
+            <p className="card-text">Description: {phone.description}</p>
+            <p className="card-text">Color: {phone.color}</p>
+            <p className="card-text">Price: ${phone.price}</p>
+            <p className="card-text">Screen: {phone.screen}</p>
+            <p className="card-text">Processor: {phone.processor}</p>
+            <p className="card-text">RAM: {phone.ram}</p>
+            <button onClick={() => navigate(-1)} className="btn btn-primary">
+              Back
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default PhonesInfo
+export default PhonesInfo;
